d55d891423acfd20620ed8867b840544f2fb0fcd
lordish <gitonga403@gmail.com>
2023-03-14 17:40:28 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
+    return "This one is on me!";
+  }
+  else if(ride>2500){
+    return "No can do.";
+   }
+   else{
+    return "I will gladly take your thirty bucks.";
+   }
+   
 }
+console.log(scuberGreetingForFeet(2001));
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ function ternaryCheckCity(city){
+    let cityName = "NYC";
+    if(city===cityName){
+      return "Ok, sounds good.";
+     } 
+     else {
+      return "No go.";
+    }
+ }
+console.log(ternaryCheckCity("Texas"));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip==="generous") {
+    return "Thank you so much."
+  }
+   else if (tip==="not as generous") {
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }
 }
+  
+console.log(switchOnCharmFromTip());
